9085d11cc16264c4f7626bf7c877914da9668735
Elisei Profir <pro.elisei@gmail.com>
2025-01-23 11:55:19 +0200

    moved to conftest.py in backend>budget

--- backend/budget/accounts/tests/conftest.py
+++ backend/budget/accounts/tests/conftest.py
@@ -2,10 +2,6 @@ import pytest
 from model_bakery import baker
 from accounts.models import User
 
-@pytest.fixture
-def user_recipe() -> str:
-    """Fixture for creating a User recipe"""
-    return "accounts.tests.user_recipe"
 
 @pytest.fixture
 def user(user_recipe) -> User:
