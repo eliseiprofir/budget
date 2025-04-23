d68444b3e9b8f4b3530a98d88ef2a3b00a6791d5
Elisei <pro.elisei@gmail.com>
2025-04-23 14:48:11 +0300

    update bucket admin view

--- backend/budget/core/admin.py
+++ backend/budget/core/admin.py
@@ -7,8 +7,7 @@ from core.models import Location
 @admin.register(Bucket)
 class BucketAdmin(admin.ModelAdmin):
     list_display = ("name", "allocation_percentage", "allocation_status", "is_removed", "user")
-    list_filter = ("is_removed",)
-    list_filter_default = {"is_removed": False}
+    list_filter = ("is_removed", "user")
     search_fields = ("name",)
     ordering = ("name",)
     readonly_fields = ("allocation_status",)
@@ -36,20 +35,10 @@ class BucketAdmin(admin.ModelAdmin):
     def get_queryset(self, request):
         return Bucket.all_objects.all()
 
-    def changelist_view(self, request, extra_context=None):
-        if not request.GET and hasattr(self, 'list_filter_default'):
-            q = request.GET.copy()
-            for key, value in self.list_filter_default.items():
-                q[key] = value
-            request.GET = q
-        return super().changelist_view(request, extra_context=extra_context)
-
-
 @admin.register(Location)
 class LocationAdmin(admin.ModelAdmin):
     list_display = ("name", "is_removed", "user")
-    list_filter = ("is_removed",)
-    list_filter_default = {"is_removed": False}
+    list_filter = ("is_removed", "user")
     search_fields = ("name",)
     ordering = ("name",)
     readonly_fields = ()
@@ -73,12 +62,4 @@ class LocationAdmin(admin.ModelAdmin):
     )
 
     def get_queryset(self, request):
-        return Location.all_objects.filter()
-
-    def changelist_view(self, request, extra_context=None):
-        if not request.GET and hasattr(self, 'list_filter_default'):
-            q = request.GET.copy()
-            for key, value in self.list_filter_default.items():
-                q[key] = value
-            request.GET = q
-        return super().changelist_view(request, extra_context=extra_context)
+        return Location.all_objects.all()
