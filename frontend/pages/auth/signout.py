5244ec87a64d27c189bfa214247591b7026b8830
Elisei <pro.elisei@gmail.com>
2025-04-29 09:25:19 +0300

    fix clear cache on signout

--- frontend/pages/auth/signout.py
+++ frontend/pages/auth/signout.py
@@ -1,6 +1,8 @@
 import time
 import streamlit as st
 
+from utils.cache_utils import clear_all_cache
+
 def signout_page():
     st.title("Sign Out")
     st.write("Click the button below to sign out.")
@@ -8,6 +10,7 @@ def signout_page():
     if st.button("Sign Out", use_container_width=True):
         st.session_state["api_auth"]["service"].logout()
         st.session_state["api_auth"]["authenticated"] = False
+        clear_all_cache()
         st.success("Sign Out successful!")
         time.sleep(1)
         st.rerun()
