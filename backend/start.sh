06d33869dd12b93be89863aff264fd228ce5c4d9
Elisei <pro.elisei@gmail.com>
2025-05-26 11:25:29 +0300

    small fix

--- backend/start.sh
+++ backend/start.sh
@@ -2,15 +2,10 @@
 
 # Run migrations
 cd budget
-echo "Making migrations..."
-python manage.py makemigrations --noinput
-echo "Migrating..."
-python manage.py migrate --noinput
+python manage.py makemigrations
+python manage.py migrate
 
-echo "Listing applied migrations:"
-python manage.py showmigrations
-
-echo "Creating default superuser if needed..."
+# Create default superuser
 python manage.py createdefaultsuperuser
 
 # Pornește aplicația
