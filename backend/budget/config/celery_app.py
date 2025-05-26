6b8dcbfff6338148c8c1fd1344094a2cf9d9a4c0
Elisei <pro.elisei@gmail.com>
2025-05-26 14:11:33 +0300

    fix celery

--- backend/budget/config/celery_app.py
+++ backend/budget/config/celery_app.py
@@ -3,7 +3,9 @@ import os
 from celery import Celery
 
 # set the default Django settings module for the 'celery' program.
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
+
+settings_module = os.environ.get("DJANGO_SETTINGS_MODULE", "config.settings.local")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
 
 app = Celery("budget")
 
